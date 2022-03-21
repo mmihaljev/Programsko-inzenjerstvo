48cd8423ad96554a5aed7b78d87999a951260139
Flaster99 <mmihaljevic.vz@gmail.com>
2022-03-21 17:59:24 +0100

    Vjezba3

---
+++ Autobusna karta/Autobusna karta/AutobusniKolodvor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autobusna_karta
+{
+    internal class AutobusniKolodvor
+    {
+        private List<Linija> _linije = new List<Linija>();
+
+        public AutobusniKolodvor()
+        {
+            _linije.Add(new Linija("Varazdin", "Zagreb", 80));
+            _linije.Add(new Linija("Varazdin", "Cakovec", 20));
+            _linije.Add(new Linija("Varazdin", "Slavonski Brod", 240));
+            _linije.Add(new Linija("Varazdin", "Krapina", 40));
+            _linije.Add(new Linija("Varazdin", "Novi Marof", 22));
+            _linije.Add(new Linija("Varazdin", "Koprivnica", 45));
+            _linije.Add(new Linija("Varazdin", "Virovitica", 100));
+            _linije.Add(new Linija("Varazdin", "Osijek", 220));
+        }
+
+        private double IzracunajCijenu(int udaljenost, string tipKarte)
+        {
+            double cijena = 0;
+            switch (tipKarte)
+            {
+                case "Regularna":
+                    cijena = udaljenost * 1.5;
+                    break;
+                case "Studentska":
+                    cijena = udaljenost * 1.2;
+                    break;
+                case "Povratna":
+                    cijena = udaljenost * 2;
+                    break;
+                default:
+                    break;
+            }
+
+            return cijena;
+        }
+
+        public string KupiKartu(string polaziste, string odrediste, string tipKarte)
+        {
+            double cijena = 0;
+
+            foreach (Linija linija in _linije)
+            {
+                if(polaziste == linija._Polaziste && odrediste == linija._Odrediste)
+                {
+                    cijena = IzracunajCijenu(linija._Udaljenost, tipKarte);
+                    break;
+                }
+            }
+
+            return $"Cijena za kartu tipa {tipKarte}, na relaciji {polaziste} - {odrediste} iznosi {cijena} kn!";
+        }
+    }
+}
---
+++ Autobusna karta/Autobusna karta/Linija.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autobusna_karta
+{
+    internal class Linija
+    {
+        public string _Polaziste, _Odrediste;
+        public int _Udaljenost;
+
+        public Linija( string polaziste, string odrediste, int udaljenost)
+        {
+            _Polaziste = polaziste;
+            _Odrediste = odrediste;
+            _Udaljenost = udaljenost;
+        }
+    }
+}
